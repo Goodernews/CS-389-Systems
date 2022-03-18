adb626b276a0c93bbf100faec7be97ee5b93109c
Taylor <40648132+Goodernews@users.noreply.github.com>
2022-03-18 12:11:27 -0700

    Delete clean.py

--- HW 9/clean.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv("Output.csv")
-
-if len(df.columns)==3:
-    df.pivot(index= "Trial", columns='Size (Bytes)', values='Time (NS)').to_csv("Output.csv")
