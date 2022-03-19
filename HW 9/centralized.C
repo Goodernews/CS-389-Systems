b5daa0c2e46c59da5d8da79b66ea96455bf9960b
Taylor <40648132+Goodernews@users.noreply.github.com>
2022-03-18 17:02:27 -0700

    Add files via upload

--- HW 9/centralized.C
+++ HW 9/centralized.C
@@ -1,16 +1,37 @@
 #include "centralized.hh"
 
-int counter = 1;
-bool sense = false;
-void barrier(P){
-	bool local_sense = ! sense;
-	if (fetch_and_increment(&counter) == P) {
-		counter = 1;
-		sense = local_sense;
+void* centralized_barrier(void *Arguments){
+	struct args *arguments = (struct args*)Arguments;
+	bool local_sense = ! arguments->Sense;
+	if (arguments->Counter.fetch_add(1) == arguments->Nthreads) {
+		arguments->Counter = 0;
+		arguments->Sense = local_sense;
 	} 
 	
 	else{
-		while (sense != local_sense){
+		while (arguments->Sense != local_sense){
 		       	/* spin */ }
 	}
+}
+
+
+int Centralized(int num_threads){
+	struct args *pass_through = (struct args *)malloc(sizeof(struct args));
+	
+	pass_through->Counter = 0;
+	pass_through->Sense = false;
+	pass_through->Nthreads = num_threads;
+	
+	pthread_t threads;
+
+
+	for(int i = 0; i<num_threads; i++){
+		pthread_create(&threads, NULL, centralized_barrier, pass_through);
+	}
+
+	return 0;
+}
+
+int main(){
+	return 0;
 }
--- HW 9/centralized.hh
+++ HW 9/centralized.hh
@@ -1,10 +1,18 @@
 #pragma once
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<atomic>
 
+using namespace std;
 
-struct Centralized{
-	// intializer that takes the num threads
-	void barrier(P);
+int Centralized(int num_threads); // implementation
 
-}
+void* centralized_barrier(void *Arguments);
+
+struct args {
+    atomic_int Counter;
+    bool Sense;
+    int Nthreads;
+};
